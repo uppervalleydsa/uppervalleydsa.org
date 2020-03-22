c0d2a9d061a2dd93586eeb61f57efe6d2485a1dd
Brian Jacobel <bjacobel@gmail.com>
2020-03-22 14:02:35 -0400

    Initial commit; gatsby boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: ['eslint-config-airbnb', 'eslint-config-prettier'],
+  rules: {
+    'max-classes-per-file': 0,
+    'no-else-return': 0,
+    'no-console': 0,
+    'import/prefer-default-export': 1,
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/order': [
+      2,
+      {
+        groups: [['builtin', 'external']],
+        'newlines-between': 'always',
+      },
+    ],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.html.jsx'] }],
+    'react/prefer-stateless-function': 0,
+    'react/destructuring-assignment': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'jsx-a11y/anchor-is-valid': [2, { specialLink: ['to'] }],
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+  },
+};
