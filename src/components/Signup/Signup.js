5111fd881a753f5382c6efab54934f5e8a7c9daa
Brian Jacobel <bjacobel@gmail.com>
2020-04-02 17:27:13 -0400

    Styling of signup form is Good Enough

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
-import { wrapper, form, row } from './signup.module.css';
+import {
+  wrapper,
+  form,
+  row,
+  emailField,
+  subBtn,
+  subTitle,
+} from './signup.module.css';
 
 export default () => {
   const group = 'upper-valley-dsa-bulletins';
   return (
     <div className={wrapper}>
-      <h4>Subscribe to our mailing list</h4>
+      <h4 className={subTitle}>Subscribe to our mailing list</h4>
       <form
         className={form}
         target="_blank"
         action={`http://groups.google.com/group/${group}/boxsubscribe`}
       >
         <div className={row}>
-          <input name="email" placeholder="Email address" />
-          <input type="submit" value="Subscribe" name="sub" />
+          <input
+            className={emailField}
+            name="email"
+            placeholder="Email address"
+          />
+          <input
+            className={subBtn}
+            type="submit"
+            value="Subscribe"
+            name="sub"
+          />
         </div>
         <div className={row}>
           <a href={`http://groups.google.com/group/${group}`}>
