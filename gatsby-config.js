093029d6c5ae424b9741129d1c3241731a06b3b2
Brian Jacobel <bjacobel@gmail.com>
2020-04-11 21:09:10 -0400

    Change default share text

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   siteMetadata: {
     title: 'Upper Valley DSA',
-    description: 'Website for the Upper Valley DSA.',
+    description:
+      'Upper Valley (Vermont and New Hampshire) chapter of the largest socialist organization in the United States.',
     author: 'Upper Valley DSA',
   },
   plugins: [
