72e643333cc5e811587c077d6720e80c228b73f3
Brian Jacobel <bjacobel@gmail.com>
2020-06-18 20:11:13 -0400

    Separate pngs for favicon and header makes the most sense

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-favicon',
       options: {
-        logo: './src/images/logo.png',
+        logo: './src/images/logo-noborder.png',
       },
     },
   ],
