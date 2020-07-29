4c6e34ae84501c3e095491223ab5d16f690eef49
Brian Jacobel <bjacobel@gmail.com>
2020-07-28 22:37:02 -0400

    blog index page has thumbnails; template works OK on mobile

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import moment from 'moment';
+import Img from 'gatsby-image/withIEPolyfill';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
@@ -8,7 +9,9 @@ import {
   list,
   date,
   headline,
+  content,
   excerpt as excerptClass,
+  thumbnail,
 } from './blog.module.css';
 
 /* eslint-disable react/no-danger */
@@ -23,8 +26,18 @@ const BlogIndex = ({ data }) => (
             <h3 className={headline}>
               <a href={fields.url}>{frontmatter.title}</a>
             </h3>
-            <h4 className={date}>{moment(frontmatter.date).format('LL')}</h4>
-            <p className={excerptClass}>{excerpt}</p>
+            <div className={content}>
+              <div className={excerptClass}>
+                <h4 className={date}>
+                  {moment(frontmatter.date).format('LL')}
+                </h4>
+                <p>{excerpt}</p>
+              </div>
+              <Img
+                className={thumbnail}
+                fluid={frontmatter.thumbnail.childImageSharp.fluid}
+              />
+            </div>
           </li>
         ),
       )}
@@ -44,6 +57,13 @@ export const query = graphql`
           frontmatter {
             title
             date
+            thumbnail {
+              childImageSharp {
+                fluid(maxWidth: 350) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
