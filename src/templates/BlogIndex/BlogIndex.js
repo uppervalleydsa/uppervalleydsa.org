9204d061735f51fea2b94f0bc23d74f730990136
Brian Jacobel <bjacobel@gmail.com>
2021-03-20 15:13:45 -0400

    blog pagination

--- src/templates/BlogIndex/BlogIndex.js
+++ src/templates/BlogIndex/BlogIndex.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql, Link, navigate } from 'gatsby';
 import moment from 'moment';
 import Img from 'gatsby-image/withIEPolyfill';
+import ReactPaginate from 'react-paginate';
+import classNames from 'classnames';
+import useMeasure from 'react-use-measure';
+import { ResizeObserver } from '@juggle/resize-observer';
 
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
+import Layout from '../../components/Layout/Layout';
+import SEO from '../../components/seo';
 import {
   list,
   date,
@@ -12,17 +16,48 @@ import {
   content,
   excerpt as excerptClass,
   thumbnail,
-} from './blog.module.css';
+  pagination,
+  selectedPage,
+  hiddenBreak,
+} from './BlogIndex.module.css';
+
+const PaginationControls = ({ numPages, currentPage }) => {
+  const [ref, bounds] = useMeasure({ polyfill: ResizeObserver });
+  const mobile = bounds.width < 750;
+  return (
+    <nav
+      className={pagination}
+      role="navigation"
+      aria-label="Pagination Navigation"
+      ref={ref}
+    >
+      <ReactPaginate
+        pageCount={numPages}
+        pageRangeDisplayed={mobile ? 1 : 3}
+        marginPagesDisplayed={mobile ? 0 : 1}
+        breakClassName={classNames({ [hiddenBreak]: mobile })}
+        previousLabel="˂"
+        nextLabel="˃"
+        initialPage={currentPage - 1}
+        containerClassName={pagination}
+        activeClassName={selectedPage}
+        onPageChange={({ selected }) => {
+          navigate(selected === 0 ? '/blog' : `/blog/${selected + 1}`);
+        }}
+      />
+    </nav>
+  );
+};
 
 /* eslint-disable react/no-danger */
-const BlogIndex = ({ data }) => (
+export default ({ data, pageContext }) => (
   <Layout>
     <SEO title="Blog" />
     <h2>Blog</h2>
     <ol className={list}>
       {data.allMarkdownRemark.edges.map(
         ({ node: { fields, frontmatter, excerpt } }) => (
-          <li>
+          <li key={fields.url}>
             <h3 className={headline}>
               <Link to={fields.url}>{frontmatter.title}</Link>
             </h3>
@@ -45,14 +80,17 @@ const BlogIndex = ({ data }) => (
         ),
       )}
     </ol>
+    <PaginationControls {...pageContext} />
   </Layout>
 );
 
 export const query = graphql`
-  query {
+  query allBlogPosts($skip: Int!, $limit: Int!) {
     allMarkdownRemark(
       filter: { fileAbsolutePath: { regex: "^/posts/" } }
       sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
     ) {
       edges {
         node {
@@ -76,5 +114,3 @@ export const query = graphql`
     }
   }
 `;
-
-export default BlogIndex;
