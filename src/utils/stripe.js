b66bcd0a3be32b577c4006abddcfefc781ba712c
Brian Jacobel <bjacobel@gmail.com>
2021-03-22 16:16:36 -0400

    Keys included in client-side js must be prefixed with GATSBY_

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -6,6 +6,6 @@ let stripeInstance;
 
 export const memoizedClient = async () => {
   if (stripeInstance) return Promise.resolve(stripeInstance);
-  stripeInstance = await loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
+  stripeInstance = await loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
   return stripeInstance;
 };
